3914b3c35ba2760573516a69380964fe3e73f1f8
Thuotracy <tracyjacobs775@gmail.com>
2022-08-01 19:10:15 +0300

    made the add button functional

---
+++ main.js
@@ -0,0 +1,10 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+})
